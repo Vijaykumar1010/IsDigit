8690d2bcb062deeecf6f0f81ddbc7c4712d51bed
Vijaykumar1010 <123072467+Vijaykumar1010@users.noreply.github.com>
2025-06-16 10:44:42 +0530

    Add files via upload

---
+++ function isDigit isnumber to return it.py
@@ -0,0 +1,25 @@
+# function isDigit isnumber to return it is a digit
+def isNumber(number: int):
+
+    isInteger = True # set a flag
+
+    for eachcharacter in number:
+
+        if (eachcharacter >='0' and eachcharacter <= '9'):
+
+            continue
+        else:
+            isInteger = False
+        break
+    
+    return isInteger
+
+# invocation code
+
+result = isNumber('0123')
+
+if result:
+    print("Yes it is an integer")
+
+else:
+    print("No it is not an integer")
